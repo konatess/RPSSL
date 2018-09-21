cfa034880e0e59fccaee00397fe6d14bdfa5e427
Kona Tess <konatess@gmail.com>
2018-09-21 14:08:11 -0700

    Got the layout mostly how I want it. Next to get some functionality.

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+// at end of each round, need to remove checked and active 
+// from the buttons, which look like
+// <label class="btn btn-secondary active">
+//    <input type="radio" name="options" id="option1" autocomplete="off" checked> Name
+// </label>
